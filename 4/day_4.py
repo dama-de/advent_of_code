306201849b162507734b6dc4b4d738bd06495a9f
Dama <dam4rusxp@googlemail.com>
2020-12-04 12:20:46 +0100

    Day 4 shorter

--- 4/day_4.py
+++ 4/day_4.py
@@ -6,7 +6,7 @@ def check_height(h):
     return (unit == "cm" and 150 <= int(height) <= 193) or (unit == "in" and 59 <= int(height) <= 76)
 
 
-def solve(inp):
+def solve(passports):
     required_fields = {"byr": lambda y: 1920 <= int(y) <= 2002,
                        "iyr": lambda y: 2010 <= int(y) <= 2020,
                        "eyr": lambda y: 2020 <= int(y) <= 2030,
@@ -15,17 +15,14 @@ def solve(inp):
                        "ecl": lambda c: c in ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"],
                        "pid": lambda i: re.fullmatch("\\d{9}", i)}
 
-    passports = inp.split("\n\n")
-
     all_present_count, all_valid_count = 0, 0
     for passport in passports:
         fields_with_value = [field.split(":") for field in re.split("[ \\n]", passport) if not field.startswith("cid")]
         existing_labels = [field[0] for field in fields_with_value]
         all_present = all(req in existing_labels for req in required_fields)
-        all_present_count = all_present_count + all_present
+        all_present_count += all_present
         try:
-            all_valid_count = all_valid_count + (all_present and all(
-                required_fields[field[0]](field[1]) for field in fields_with_value))
+            all_valid_count += (all_present and all(required_fields[field[0]](field[1]) for field in fields_with_value))
         except:
             continue
 
@@ -34,6 +31,6 @@ def solve(inp):
 
 if __name__ == '__main__':
     fin = open("input.txt", "r")
-    text = fin.read()
+    text = fin.read().split("\n\n")
 
     print(solve(text))
